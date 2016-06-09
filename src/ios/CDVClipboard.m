e55f420b4bc81666e3ce8ad82dd61bd63899740f
ericzhao0825 <ezhao@brainfuse.com>
2016-06-09 13:03:57 -0400

    add image pasting ios support.

--- src/ios/CDVClipboard.m
+++ src/ios/CDVClipboard.m
@@ -19,10 +19,32 @@
 
 - (void)paste:(CDVInvokedUrlCommand*)command {
 	[self.commandDelegate runInBackground:^{
-		UIPasteboard *pasteboard = [UIPasteboard generalPasteboard];
-		NSString     *text       = [pasteboard valueForPasteboardType:@"public.text"];
-	    
-	    CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:text];
+
+        UIPasteboard *pasteboard = [UIPasteboard generalPasteboard];
+
+        NSString *data = @"";
+        NSData *imageData;
+        NSArray *types = @[@"public.text",@"public.jpeg",@"public.png"];
+        
+        if([[pasteboard pasteboardTypes] containsObject:types[0]]){
+            
+             data = [pasteboard valueForPasteboardType:types[0]];
+            
+        }else if([[pasteboard pasteboardTypes] containsObject:types[1]]){
+            
+            imageData = [pasteboard dataForPasteboardType:types[1]];
+            
+        }else if([[pasteboard pasteboardTypes] containsObject: types[2]]){
+            
+            imageData = [pasteboard dataForPasteboardType:types[2]];
+        }
+        
+        if(imageData != nil){
+            data =  [NSHomeDirectory() stringByAppendingPathComponent:@"Documents/image.jpg"];
+            [imageData writeToFile:data atomically:YES];
+        }
+      	    
+	    CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:data];
 	    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
 	}];
 }
